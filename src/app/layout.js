4850f6fa97e699a29fd407a72fc7ddf43aa8e9eb
ElFabiux <fabiargue33@gmail.com>
2026-04-16 18:13:49 -0600

    title added

---
+++ src/app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Lion's Auto Detailing",
+  description: 'Servicio profesional de detailing automotriz en Santiago, Chile.',
+}
+
+export default function RootLayout({ children }) {
+  return children
+}
